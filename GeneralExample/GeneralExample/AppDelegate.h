9068fae128013b1b9da255a72ade1284b75bfc10
pxh <peixinhua@unisound.com>
2018-05-16 18:40:48 +0800

    save clean project

---
+++ GeneralExample/GeneralExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GeneralExample
+//
+//  Created by pxh on 2018/5/16.
+//  Copyright © 2018年 pxh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GeneralExample/GeneralExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GeneralExample
+//
+//  Created by pxh on 2018/5/16.
+//  Copyright © 2018年 pxh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
