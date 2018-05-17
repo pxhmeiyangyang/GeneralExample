5dac5c0e309f5e543acbe5a35d9e20aa8cd8c2b3
pxh <peixinhua@unisound.com>
2018-05-17 18:54:10 +0800

    update general example UI

--- GeneralExample/GeneralExample/ViewController.m
+++ GeneralExample/GeneralExample/ViewController.m
@@ -8,23 +8,62 @@
 
 #import "ViewController.h"
 #import "FMDBManager.h"
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
 
+@property(nonatomic,strong)UITableView* tableView;
+
+@property(nonatomic,strong)NSDictionary* data;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    [FMDBManager creatTableWithTableName:@"Person" arFields:@[@"name",@"age",@"sex"] arFieldsType:@[@"text",@"text",@"text"]];
+    self.navigationItem.title = @"开发示例";
+//    [FMDBManager creatTableWithTableName:@"Person" arFields:@[@"name",@"age",@"sex"] arFieldsType:@[@"text",@"text",@"text"]];
+    self.data = @{@"数据存储":@[@"FMDB"]};
+    [self configureTableview];
 }
 
+- (void)configureTableview{
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:self.tableView];
+    self.tableView.backgroundColor = UIColor.lightGrayColor;
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.tableFooterView = [[UIView alloc] init];
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+//MARK:--UITableViewDelegate,UITableViewDataSource
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return self.data.count;
+}
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    return self.data.allKeys[section];
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    NSArray* array = (NSArray* )[self.data allValues][section];
+    return array.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+    NSArray* array = (NSArray* )[self.data allValues][indexPath.section];
+    cell.textLabel.text = array[indexPath.row];
+    return cell;
+}
 
 @end
