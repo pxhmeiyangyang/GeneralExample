1f5d031dcc93462b2d8908212f609ad5e2d54f02
pxh <peixinhua@unisound.com>
2018-06-26 19:06:41 +0800

    update wcdb manager

--- GeneralExample_swift/GeneralExample/GeneralExample/DB/WCDBManager.swift
+++ GeneralExample_swift/GeneralExample/GeneralExample/DB/WCDBManager.swift
@@ -16,45 +16,113 @@ let dataBase = Database.init(withPath: WCDBPath())
 
 let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first! as NSString
 
-/// 升级数据库版本更新所有表
-func WCDBUpdatedVersion(){
-    guard let version = UserDefaults.standard.string(forKey: kWcdbVersionKey) else {
-        WCDBUpdateAllTables()
-        return
-    }
-    if version != UserDefaults.standard.string(forKey: kWcdbVersionKey) {
-        WCDBUpdateAllTables()
-    }
-}
-
-func WCDBUpdateAllTables(){
-    do {
-        try dataBase.create(table: "WCDBPerson", of: WCDBPerson.self)
-    } catch  {
-        
-    }
-}
-
 func WCDBPath()->String{
     return documentPath.appendingPathComponent("wcdb.db")
 }
 
+class WCDBManager{
+    
+    /// 数据库是否需要升级版本
+    ///
+    /// - Returns: 返回是否需要的bool值
+    class func isNeedUpdated()->Bool{
+        guard let version = UserDefaults.standard.string(forKey: kWcdbVersionKey) else {
+            return true
+        }
+        if version != UserDefaults.standard.string(forKey: kWcdbVersionKey) {
+            return true
+        }
+        return false
+    }
+    
+    class func update<T: TableCodable>(table: T.Type){
+        let tableName = String(describing: table)
+        guard let judge = try? dataBase.isTableExists(tableName) else {
+            create(table: table)
+            return
+        }
+        if isNeedUpdated() || !judge {
+            create(table: table)
+        }
+    }
+    
+    class func create<T: TableCodable>(table: T.Type){
+        do {
+            let tableName = String(describing: table)
+            try dataBase.create(table: tableName, of: table)
+        } catch {
+            
+        }
+    }
+    
+    class func insertOrReplace<T: TableCodable>(table: T,tableType: T.Type){
+        update(table: tableType)
+        do {
+            let tableName = String(describing: table)
+            try dataBase.insertOrReplace(objects: [table], intoTable: tableName)
+        } catch {
+            
+        }
+    }
+    
+}
+
+func test(){
+    let person = WCDBPerson()
+    try? dataBase.insertOrReplace(objects: person, intoTable: "person")
+}
+
+
 class WCDBPerson: WCDBSwift.TableCodable {
     //Your own properties
-    let variable1: Int = 0
-    var variable2: String? // Optional if it would be nil in some WCDB selection
-    var variable3: Double? // Optional if it would be nil in some WCDB selection
-    let unbound: Date? = nil
+    var id: Int? // Optional if it would be nil in some WCDB selection
+    var name: String? // Optional if it would be nil in some WCDB selection
+    var age: Int?
+    var gener: String?
+    var address: String?
+    var car : WCDBCar?
     
     enum CodingKeys: String, CodingTableKey {
         typealias Root = WCDBPerson
         
         //List the properties which should be bound to table
-        case variable1 = "custom_name"
-        case variable2
-        case variable3
+        case id
+        case name
+        case age
+        case gener
+        case address
+        case car
         
         static let objectRelationalMapping = TableBinding(CodingKeys.self)
         
+        static var columnConstraintBindings: [CodingKeys: ColumnConstraintBinding]? {
+            return [
+                id: ColumnConstraintBinding(isPrimary: true)
+            ]
+        }
+        
     }
 }
+
+
+class WCDBCar: WCDBSwift.TableCodable {
+    //Your own properties
+    
+    var brand: String? // Optional if it would be nil in some WCDB selection
+    var name: String? // Optional if it would be nil in some WCDB selection
+    var number: String?
+    
+    enum CodingKeys: String, CodingTableKey {
+        typealias Root = WCDBCar
+        
+        //List the properties which should be bound to table
+        case brand
+        case name
+        case number
+        
+        static let objectRelationalMapping = TableBinding(CodingKeys.self)
+        
+        
+    }
+}
+
--- README.md
+++ README.md
@@ -9,3 +9,22 @@ UI布局</br>
 UI布局</br>
 数据库：WCDB.swift</br>
 数据对象解析：objectMapper</br>
+
+
+### 数据库示例逻辑
+根据版本自动更新</br>
+创建一个person对象，有增删改查的逻辑，person示例如下</br>
+class Person{
+> var  id : int //primaryKey
+> var name : string
+> var age : int
+> var gender : string
+> var address : string
+> var car : Car
+}
+
+class Car{
+> var brand : string
+> var name : string
+> var number : string
+}
